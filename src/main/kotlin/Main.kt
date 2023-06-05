464f9fc052787c618f9d49cf6d8ac32881cce8b1
DiamondTaco <imadiamondtaco@gmail.com>
2023-06-05 02:43:29 -0500

    Update main to reflect commandparser

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,32 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+operator fun <T> List<T>.component6() = this[5]
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
 
-    val intArgument = Argument<Int, Nothing?>()
-        .withName("huge_balls")
-        .withShort('h')
-        .withParser(object : Parser<Int, Nothing?> {
-            override fun parse(token: String) = 35
-        })
-        .withDefault(22)
-        .withDefaultSupplier { 28 }
+fun main() {
+    val parser = CommandParser<Unit>()
 
-    println(intArgument)
+    val emptyArg = Argument(object : Parser<Nothing, Unit> {
+        override fun getCompletions(typed: String, context: Unit): List<String> {
+            TODO("Not yet implemented")
+        }
+
+        override fun parse(token: String): Nothing {
+            TODO("Not yet implemented")
+        }
+
+    })
+
+    parser.addArgument(Flag("fjeesder", 'f'), emptyArg)
+    parser.addArgument(Flag("djesset", 'j'), emptyArg)
+    parser.addArgument(Flag("ssle", 'e'), emptyArg)
+    parser.addFlag(Flag("a_", 'a'))
+    parser.addFlag(Flag("b_", 'b'))
+    parser.addFlag(Flag("c_", 'c'))
+
+    val command = "--aot=3d -aet  --fjee=`{sdfkj sdkfj sldfj sjdj`"
+
+    val x = parser.parsePartial(command, Unit, 11)
+    val y = parser.parsePartial(command, Unit, 19)
+
+
+    println("$x @ ${command[11]}, $y @ ${command[17]}")
 }
