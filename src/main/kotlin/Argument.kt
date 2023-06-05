29f28065e6aac20a44912c91049567ea81b06a6f
DiamondTaco <imadiamondtaco@gmail.com>
2023-06-05 02:42:33 -0500

    Make it into a data class

--- src/main/kotlin/Argument.kt
+++ src/main/kotlin/Argument.kt
@@ -1,17 +1,1 @@
-class Argument<T, C> {
-    lateinit var longName: String
-    lateinit var parser: Parser<T, C>
-    var shortName: String? = null
-    var defaultSupplier: (() -> T)? = null
-
-    fun withName(longName: String) = apply { this.longName = longName }
-
-    fun withShort(shortName: Char) = apply { this.shortName = shortName.toString() }
-
-    fun withParser(parser: Parser<T, C>) = apply { this.parser = parser }
-
-    fun withDefault(default: T) = apply { this.defaultSupplier = { default } }
-
-    fun withDefaultSupplier(defaultSupplier: () -> T) = apply { this.defaultSupplier = defaultSupplier }
-}
-
+data class Argument<T, C>(val parser: Parser<T, C>, val defaultSupplier: (() -> T)? = null)
