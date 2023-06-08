57e5e243109b9714ae60b96eac285c67cb86d8a2
DiamondTaco <imadiamondtaco@gmail.com>
2023-06-07 23:46:43 -0500

    Fix Regex.matchAll

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,17 @@
+import parsers.DoubleParser
 import parsers.IntParser
 import parsers.Parser
-import com.mojang.brigadier.CommandDispatcher
 
 operator fun <T> List<T>.component6() = this[5]
 
 
 fun main() {
-    val dispatcher = CommandDispatcher<Int>()
-
-
     testStuff()
 }
 
 fun testStuff() {
 
     val parser = CommandParser<Unit>()
-
-
 
 
     val emptyArg = Argument(object : Parser<Nothing, Unit> {
@@ -30,13 +25,13 @@ fun testStuff() {
     })
 
 
-
     parser.addArgument(Flag("fjeesder", 'f'), Argument(IntParser()))
     parser.addArgument(Flag("djesset", 'j'), emptyArg)
-    parser.addArgument(Flag("ssle", 'e'), emptyArg)
+    parser.addArgument(Flag("aot", 'p'), Argument(DoubleParser()))
     parser.addFlag(Flag("a_", 'a'))
     parser.addFlag(Flag("b_", 'b'))
     parser.addFlag(Flag("c_", 'c'))
+    parser.addFlag(Flag("t_", 't'))
 
     val command = "--aot=3d -aet  --fjeesder=3352"
 
@@ -44,24 +39,18 @@ fun testStuff() {
     val y = parser.parsePartial(command, Unit, 19)
     val z = parser.parsePartial(command, Unit, 29)
 
-    val commandB = "--fjeesder=3352"
+    val commandB = "--aot=234.2193 -abt  --fjeesder=3352"
     val parsed = parser.parseCommand(commandB)
 
     println(parsed)
-
 
     println("$x @ ${command[11]}, $y @ ${command[19]}, $z @ ${command[29]}")
 }
 
 fun Regex.matchAll(input: CharSequence): List<MatchResult>? {
-    var currentIndex = 0
-    val matches = mutableListOf<MatchResult>()
+    val matches = generateSequence(matchAt(input, 0)) { it.next() }.toList()
 
-    while (currentIndex < input.length) {
-        (matchAt(input, currentIndex) ?: return null)
-            .apply { currentIndex = range.last + 1}
-            .also { matches.add(it) }
-    }
+    if (matches.lastOrNull()?.run { range.last < input.length } != false) return null
 
     return matches
 }
