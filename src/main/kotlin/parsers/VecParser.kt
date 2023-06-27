b6111898bf361abffa134a2c1821ba7d7239d4da
DiamondTaco <imadiamondtaco@gmail.com>
2023-06-27 02:46:29 -0500

    Bugfix

--- src/main/kotlin/parsers/VecParser.kt
+++ src/main/kotlin/parsers/VecParser.kt
@@ -5,21 +5,24 @@ import matchAll
 
 class VecParser<T, C>(private val parser: Parser<T, C>, private val length: Int? = null) : Parser<List<T>, C> {
 
-    private val parseRegex = Regex("(`.+?`|[^,]+),? *")
+    // matches `a`,| `a`,   | as df abc, | `a,a`
+    private val listRegex = Regex("(`.+?`|[^,]+),? *")
 
     override fun getCompletions(typed: String, context: C): List<String> {
         if (typed.isEmpty()) return emptyList()
 
-        val splitTyped = typed.split(',')
+        val splitTyped = listRegex.matchAll(typed).toList()
 
         if (length != null && splitTyped.size > length) return emptyList()
 
-        return parser.getCompletions(splitTyped.last(), context)
+        val lastTyped = splitTyped.lastOrNull()?.groupValues?.get(1) ?: ""
+
+        return parser.getCompletions(lastTyped, context)
             .map { it + if (splitTyped.size == length) "" else ", " }
     }
 
     override fun parse(token: String): List<T> {
-        val splitToken = parseRegex.matchAll(token) ?: throw ParseException("Could not split input into list")
+        val splitToken = listRegex.matchAll(token).toList()
 
         if (length != null && splitToken.size != length) {
             throw ParseException("List should be of length $length, was ${splitToken.size}")
