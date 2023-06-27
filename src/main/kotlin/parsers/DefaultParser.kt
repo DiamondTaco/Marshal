534ffa6b034de383e6ee271f7f7ab2b2418da17f
DiamondTaco <imadiamondtaco@gmail.com>
2023-06-27 02:45:35 -0500

    Clean up DefaultParser (it will probably be deleted later)

--- src/main/kotlin/parsers/DefaultParser.kt
+++ src/main/kotlin/parsers/DefaultParser.kt
@@ -3,12 +3,10 @@ package parsers
 import ParseException
 
 class DefaultParser<T, C>(val parser: Parser<T, C>, val defaultSupplier: () -> T) : Parser<T, C> {
-    override fun getCompletions(typed: String, context: C): List<String> =
-        parser.getCompletions(typed, context)
-    override fun parse(token: String): T =
-        try {
-            parser.parse(token)
-        } catch (e: ParseException) {
-            defaultSupplier()
-        }
+    override fun getCompletions(typed: String, context: C): List<String> = parser.getCompletions(typed, context)
+    override fun parse(token: String): T = try {
+        parser.parse(token)
+    } catch (e: ParseException) {
+        defaultSupplier()
+    }
 }
